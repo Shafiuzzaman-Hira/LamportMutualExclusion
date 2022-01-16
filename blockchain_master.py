b58402d0b2102787130e52d1233ce8bbc3f6a86e
Md Shafiuzzaman <md.shafiuzzaman.hira@gmail.com>
2022-01-16 01:37:41 -0800

    server serves multiple requests

--- blockchain_master.py
+++ blockchain_master.py
@@ -5,14 +5,18 @@ import socket
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind((HOST, PORT))
+
+while True:
+    sock.listen()
+    conn, address = sock.accept()
+    print('Connected by', address)
+    try:
             data = conn.recv(1024)
             if not data:
                 break
             conn.sendall(data)
+
+    finally:
+        conn.close()
