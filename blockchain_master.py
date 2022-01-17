34e9e1e4d560b5e7614d65fcfff2d4a5e5f7826b
Md Shafiuzzaman <md.shafiuzzaman.hira@gmail.com>
2022-01-17 12:50:57 -0800

    request menu added

--- blockchain_master.py
+++ blockchain_master.py
@@ -6,6 +6,7 @@ HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((HOST, PORT))
 
 while True:
@@ -14,6 +15,7 @@ while True:
     print('Connected by', address)
     try:
         request = conn.recv(1024)
+        print(request)
         response = "$10" + str(request)
         res = bytes(response, 'utf-8')
         conn.sendall(res)
--- client.py
+++ client.py
@@ -2,16 +2,49 @@ import socket
 import random
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+PORT = 65432  # The port used by the server
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_id = random.randint(10001,99999)
-client_address = (HOST, PORT)
-request = str(client_id)
-sock.connect(client_address)
-request= bytes(request, 'utf-8')
-sock.sendall(request)
+server_address = (HOST, PORT)
+sock.connect(server_address)
+
+transaction_menu = {"Transfer:1", "Balance:2"}
+
+while True:
+    print(transaction_menu)
+    user_command = int(input("Your selection: "))
+    if user_command == 1:
+        receiver_id = input("Receiver id:")
+        transfer_amount = input("Transfer amount:")
+        query_string = "1 " + receiver_id + " " + transfer_amount
+        #client_ui.send(query_string)
+        request = bytes(query_string, 'utf-8')
+        sock.sendall(request)
+        response = sock.recv(1024)
+        print(response)
+        if int(response[0]) == '0':
+            print("INCORRECT")
+        else:
+            print("SUCCESS")
+
+    elif user_command == 2:
+        query_string = str(2)
+        #client_ui.send(query_string)
+        request = bytes(query_string, 'utf-8')
+        sock.sendall(request)
+        #response = client_ui.recv()
+        #print(response)
+
+    else:
+        print("Invalid command, Please try again")
+        continue
+
 
 data = sock.recv(1024)
-
 print('Received', repr(data))
+sock.close()
+
+
+
+
+
